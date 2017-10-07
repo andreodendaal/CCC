53ca761d32ccef233faf4baddb45bef0441035c0
andreodendaal <aodendaal.direct@gmail.com>
2017-10-07 14:44:15 -0500

    lap processor

--- lap_processor.py
+++ lap_processor.py
@@ -11,33 +11,43 @@ class Lap_processor:
         # str2date = lambda x: datetime.strptime(x.decode("utf-8"), '%H:%M:%S')
 
         race = np.genfromtxt(filename, delimiter=",", dtype=str)
-        #print(race)
+        print("printing race file...")
+        print(race)
         racer_laps = race[1: , 7:]
-        #print(racer_laps)
-        laps = racer_laps.T
-        #print(laps[0])
 
+        print("printing laps file by racer")
+        print(racer_laps)
+
+        laps = racer_laps.transpose()
+
+        print("lap formatting tests")
         print(laps[0][0])
         lap_value = laps[0][0]
         lap_value_time = (datetime.strptime(str(lap_value), '%M:%S')).time()
         print(lap_value_time)
+        # create grid of times per laps
 
-        laps_timed = [(datetime.strptime(str(x), '%M:%S')).time() for x in laps[0]]
+        laps_timed = [(datetime.strptime(str(x), '%M:%S')).time() for x in laps[0] if x != '']
         print(laps_timed[0])
+        print("\n Lap times grid formatted to time object....")
         print(laps_timed)
+
+        # sort laps for reference
+        print("\n Lap times grid formatted sorted...")
         laps_timed.sort()
         print(laps_timed)
 
+        #lap_times = racer_laps.transpose()
 
-
-
-
-         #lap_times = racer_laps.transpose()
-
-        #print(lap_times[0])
+        for i in racer_laps:
+            for t in i:
+                if t != '':
+                    #print(t)
+                    t_time = (datetime.strptime(str(t), '%M:%S')).time()
+                    print(t_time in laps_timed)
 
 
 
 if __name__ == '__main__':
 
-    my_lap = Lap_processor("2017 CCC Lap Times - Hopkins Park.csv")
+    my_lap = Lap_processor("test.csv")
